0a54743685d0c07c0f23a0de6e0fd9f21bd894b3
bertinmick69 <bertinmick@yahoo.fr.com>
2024-03-21 00:33:01 +0100

    props.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import PokemonCard from "./components/pokemonCard";
 import "./App.css";
+import PokemonCard from "./components/pokemonCard";
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
     </div>
   );
 }
--- src/components/pokemonCard.jsx
+++ src/components/pokemonCard.jsx
@@ -1,12 +1,14 @@
-function PokemonCard() {
+function PokemonCard({ pokemon }) {
   return (
-    <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
+    <figure className="card">
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
+
 export default PokemonCard;
